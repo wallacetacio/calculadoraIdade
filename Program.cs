a4fccbcc309929e9bd2824cc0667e7805357d6b5
Wallace Tácio <wallacett.dev@gmail.com>
2022-08-29 11:14:17 -0300

    Adiciona solução a atividade Calcular Idade FATEC 2022

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+double anoNascimento, anoAtual, idade;
+
+Console.Write("Digite o ano de nascimento: ");
+anoNascimento = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o ano atual: ");
+anoAtual = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("");
+
+idade = anoAtual - anoNascimento;
+
+Console.WriteLine("A idade é: " + idade);
+Console.ReadKey();
